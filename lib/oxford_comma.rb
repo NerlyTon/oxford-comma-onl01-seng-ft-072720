385be3e4f1c419935a9c47b2e3896164cbbe40b6
Nerly Ton <nerlyton@gmail.com>
2020-07-30 19:55:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   return array.join()
   elsif array.length == 2
    return array.join(" and ")
-  elsif array.length > 2
-    return array.join(" and " array.length == -2) << " and"
+  else array.length > 2
+    array.length == -2 << " and"
    end
 end
