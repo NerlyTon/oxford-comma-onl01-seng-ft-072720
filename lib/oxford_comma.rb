396556249ec64838f20013e64b045e3818044757
Nerly Ton <nerlyton@gmail.com>
2020-07-30 19:51:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   return array.join()
   elsif array.length == 2
    return array.join(" and ")
-  elsif array.length == 3
+  elsif array.length > 2
     return array.join(" and ") << " and"
    end
 end
