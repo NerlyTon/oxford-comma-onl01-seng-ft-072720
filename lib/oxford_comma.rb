03540e394440c05971b1a88dc29693aa257c73f1
Nerly Ton <nerlyton@gmail.com>
2020-07-30 19:59:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2
    return array.join(" and ")
   else array.length > 2
-    return array.join == array.insert(-2, 'and')
+    return array.join << array.insert(-2, 'and')
     end
 end
