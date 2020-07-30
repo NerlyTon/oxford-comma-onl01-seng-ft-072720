8c1c17282d32ed0be5d186bdb9484805898adb05
Nerly Ton <nerlyton@gmail.com>
2020-07-30 20:48:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
    return array.join(" and ")
   else array.length > 2
    newstring = array.join(",")  
-    return newstring.insert(-2, 'and')
+    return newstring.insert(-7, 'and')
     end
 end
