1130a668343c96850cf0ce913a55eb0d9dfdd632
Nerly Ton <nerlyton@gmail.com>
2020-07-30 20:51:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
    return array.join(" and ")
   else array.length > 2
    newstring = array.join(",")  
-    return newstring.push(and)
+    return newstring.push('and')
     end
 end
